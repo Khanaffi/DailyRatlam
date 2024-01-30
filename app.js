11c0cdcd2fcb8ea4d4ac9cecdbff15a6d3004609
Aftab khan <aftabkhan0317@gmail.com>
2024-01-31 00:24:22 +0530

    final

--- app.js
+++ app.js
@@ -5,47 +5,70 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _= require('lodash');
 const mongoose= require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect("mongodb+srv://aftabkhan0317:3wePa6drkULxvNIt@cluster0.scxjv7h.mongodb.net/blogDB ", { useNewUrlParser: true })
+const app = express();
+
+
+let port = process.env.PORT;
+
+
+
+mongoose.connect(process.env.URL,{ useNewUrlParser: true }).then(()=>{
+
+
+// if(port==null||port==""){
+//     port=3000;
+// }
+app.listen(port||3000, function () {
+    console.log('sarver is started');
+});
+}).catch((err)=>console.log(err));
 
 
  
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 
-const postschema= new mongoose.Schema({title:String,body:String});
+const postschema= new mongoose.Schema({title:String,body:String,Imgtext:String,Head2:String,Imgtext2:String,body2:String, updated: { type: Date, default: Date.now },});
 
 const Post=mongoose.model('post' ,postschema);
 
-const postone=new Post({titile:"hiii",body:"this is the test post"})
+// const postone=new Post({title:"hiii",body:"this is the test post",Imgtext:"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"})
 // postone.save();
-const homeStartingContent = "hii am affi , this is my personal blog page, keeep reading thankyou";
+const homeStartingContent = "HEY Buddy ,Welcome to my Page here you got all information About Ratlam , So Keep Visiting and Keep Reading ThankYou ❤️ ";
 const aboutContent = "https://khanaffi.github.io/mysite/"
-const contactContent = "aftabkhan0317@gmail.com"
-
-const app = express();
-
+const contactContent = "Dailyratlam0@gmail.com"
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 // const thedata=[]
 
 
-app.get("/",function(req,res){
+app.get("/", async (req,res)=>{
+//  let datap=[title="hii",body="hlw"];
 
- 
-  Post.find( {},function(err,posts){
+// function(err,posts){
+  const postdata= await Post.find({}).sort({updated:-1});
+  res.render("home",{inner:homeStartingContent,newpost:postdata});
+
+  });
+//   try {
     
-    res.render("home",{inner:homeStartingContent,newpost:posts});
-  })
+// } catch (error) {
+//   res.status(404).json({ message: error.message });
+//   res.render("home",{inner:homeStartingContent,newpost:{}});
+
+// });
+
   
  
-  
-  
-})
 
 app.get("/about",function(req,res){
-  res.render("about",{innerab:aboutContent});
+  // res.render("about",{innerab:aboutContent});
+  // res.sendFile(__dirname + '/about.html');
+  res.redirect("https://khanaffi.github.io/mysite/")
 })
 app.get("/compose",function(req,res){
   res.render("compose");
@@ -53,7 +76,11 @@ app.get("/compose",function(req,res){
 app.post("/compose",function(req,res){
   const post = new Post ({
     title: req.body.usertext,
-    body: req.body.postbody
+    body: req.body.postbody,
+    Imgtext: req.body.imgtext,
+    Head2:req.body.usertext2,
+    Imgtext2: req.body.imgtext2,
+    body2:req.body.postbody2
   });
   post.save(function(err){
 
@@ -62,17 +89,20 @@ app.post("/compose",function(req,res){
       res.redirect("/");
  
     }
- 
   });
 })
 app.get("/posts/:testings",function(req,res){
   const reqdata =req.params.testings;
 
 
-  Post.findOne({title:reqdata}, function(err, post){
+  Post.findOne({"title":reqdata}, function(err, post){
     res.render("post", {
       title: post.title,
-      content: post.body
+      content: post.body,
+      Imgtext: post.Imgtext,
+      Head2:post.Head2,
+      Imgtext2:post.Imgtext2,
+      body2:post.body2
     });
   });
 
@@ -81,6 +111,9 @@ app.get("/posts/:testings",function(req,res){
 app.get("/contact",function(req,res){
   res.render("contact",{innerct:contactContent});
 })
+app.get("/blog",function(req,res){
+  res.render("blog",{innerct:contactContent});
+})
 
 
 
@@ -93,10 +126,4 @@ app.get("/contact",function(req,res){
 
 
 
-let port = process.env.PORT;
-// if(port==null||port==""){
-//     port=3000;
-// }
-app.listen(port||3000, function () {
-    console.log('started');
-});
+
